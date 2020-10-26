3cc3b4395b9010bc1412a0da0fb9e1c16fd0a5d9
Daniel Popescu <danielpops@gmail.com>
2020-10-25 17:35:44 -0700

    Include plan change summary for successful plans that use the "wrapped" template. Fixes 1223

--- server/events/markdown_renderer_test.go
+++ server/events/markdown_renderer_test.go
@@ -135,6 +135,7 @@ func TestRenderProjectResults(t *testing.T) {
 				{
 					PlanSuccess: &models.PlanSuccess{
 						TerraformOutput: "terraform-output",
+						ChangeSummary:   "Plan: 1 to add, 2 to change, 3 to destroy.",
 						LockURL:         "lock-url",
 						RePlanCmd:       "atlantis plan -d path -w workspace",
 						ApplyCmd:        "atlantis apply -d path -w workspace",
@@ -170,6 +171,7 @@ $$$
 				{
 					PlanSuccess: &models.PlanSuccess{
 						TerraformOutput: "terraform-output",
+						ChangeSummary:   "Plan: 1 to add, 2 to change, 3 to destroy.",
 						LockURL:         "lock-url",
 						RePlanCmd:       "atlantis plan -d path -w workspace",
 						ApplyCmd:        "atlantis apply -d path -w workspace",
@@ -208,6 +210,7 @@ $$$
 				{
 					PlanSuccess: &models.PlanSuccess{
 						TerraformOutput: "terraform-output",
+						ChangeSummary:   "Plan: 1 to add, 2 to change, 3 to destroy.",
 						LockURL:         "lock-url",
 						RePlanCmd:       "atlantis plan -d path -w workspace",
 						ApplyCmd:        "atlantis apply -d path -w workspace",
@@ -285,6 +288,7 @@ $$$
 					RepoRelDir: "path",
 					PlanSuccess: &models.PlanSuccess{
 						TerraformOutput: "terraform-output",
+						ChangeSummary:   "Plan: 1 to add, 2 to change, 3 to destroy.",
 						LockURL:         "lock-url",
 						ApplyCmd:        "atlantis apply -d path -w workspace",
 						RePlanCmd:       "atlantis plan -d path -w workspace",
@@ -295,6 +299,7 @@ $$$
 					RepoRelDir:  "path2",
 					ProjectName: "projectname",
 					PlanSuccess: &models.PlanSuccess{
+						ChangeSummary:   "Plan: 1 to add, 2 to change, 3 to destroy.",
 						TerraformOutput: "terraform-output2",
 						LockURL:         "lock-url2",
 						ApplyCmd:        "atlantis apply -d path2 -w workspace",
@@ -421,6 +426,7 @@ $$$
 					RepoRelDir: "path",
 					PlanSuccess: &models.PlanSuccess{
 						TerraformOutput: "terraform-output",
+						ChangeSummary:   "Plan: 1 to add, 2 to change, 3 to destroy.",
 						LockURL:         "lock-url",
 						ApplyCmd:        "atlantis apply -d path -w workspace",
 						RePlanCmd:       "atlantis plan -d path -w workspace",
@@ -607,6 +613,7 @@ func TestRenderProjectResultsDisableApplyAll(t *testing.T) {
 				{
 					PlanSuccess: &models.PlanSuccess{
 						TerraformOutput: "terraform-output",
+						ChangeSummary:   "Plan: 1 to add, 2 to change, 3 to destroy.",
 						LockURL:         "lock-url",
 						RePlanCmd:       "atlantis plan -d path -w workspace",
 						ApplyCmd:        "atlantis apply -d path -w workspace",
@@ -638,6 +645,7 @@ $$$
 				{
 					PlanSuccess: &models.PlanSuccess{
 						TerraformOutput: "terraform-output",
+						ChangeSummary:   "Plan: 1 to add, 2 to change, 3 to destroy.",
 						LockURL:         "lock-url",
 						RePlanCmd:       "atlantis plan -d path -w workspace",
 						ApplyCmd:        "atlantis apply -d path -w workspace",
@@ -672,6 +680,7 @@ $$$
 					RepoRelDir: "path",
 					PlanSuccess: &models.PlanSuccess{
 						TerraformOutput: "terraform-output",
+						ChangeSummary:   "Plan: 1 to add, 2 to change, 3 to destroy.",
 						LockURL:         "lock-url",
 						ApplyCmd:        "atlantis apply -d path -w workspace",
 						RePlanCmd:       "atlantis plan -d path -w workspace",
@@ -683,6 +692,7 @@ $$$
 					ProjectName: "projectname",
 					PlanSuccess: &models.PlanSuccess{
 						TerraformOutput: "terraform-output2",
+						ChangeSummary:   "Plan: 1 to add, 2 to change, 3 to destroy.",
 						LockURL:         "lock-url2",
 						ApplyCmd:        "atlantis apply -d path2 -w workspace",
 						RePlanCmd:       "atlantis plan -d path2 -w workspace",
@@ -1099,6 +1109,7 @@ func TestRenderProjectResults_WrapSingleProject(t *testing.T) {
 							Workspace:  "default",
 							PlanSuccess: &models.PlanSuccess{
 								TerraformOutput: c.Output,
+								ChangeSummary: "Plan: 1 to add, 2 to change, 3 to destroy.",
 								LockURL:         "lock-url",
 								RePlanCmd:       "replancmd",
 								ApplyCmd:        "applycmd",
@@ -1122,6 +1133,7 @@ func TestRenderProjectResults_WrapSingleProject(t *testing.T) {
 						if c.ShouldWrap {
 							exp = `Ran Plan for dir: $.$ workspace: $default$
 
+` + "Change Summary: `Plan: 1 to add, 2 to change, 3 to destroy.`" + `
 <details><summary>Show Output</summary>
 
 $$$diff
@@ -1247,6 +1259,7 @@ func TestRenderProjectResults_MultiProjectPlanWrapped(t *testing.T) {
 				Workspace:  "staging",
 				PlanSuccess: &models.PlanSuccess{
 					TerraformOutput: tfOut,
+					ChangeSummary:   "Plan: 1 to add, 2 to change, 3 to destroy.",
 					LockURL:         "staging-lock-url",
 					ApplyCmd:        "staging-apply-cmd",
 					RePlanCmd:       "staging-replan-cmd",
@@ -1257,6 +1270,7 @@ func TestRenderProjectResults_MultiProjectPlanWrapped(t *testing.T) {
 				Workspace:  "production",
 				PlanSuccess: &models.PlanSuccess{
 					TerraformOutput: tfOut,
+					ChangeSummary:   "Plan: 1 to add, 2 to change, 3 to destroy.",
 					LockURL:         "production-lock-url",
 					ApplyCmd:        "production-apply-cmd",
 					RePlanCmd:       "production-replan-cmd",
@@ -1270,6 +1284,7 @@ func TestRenderProjectResults_MultiProjectPlanWrapped(t *testing.T) {
 1. dir: $.$ workspace: $production$
 
 ### 1. dir: $.$ workspace: $staging$
+` + "Change Summary: `Plan: 1 to add, 2 to change, 3 to destroy.`" + `
 <details><summary>Show Output</summary>
 
 $$$diff
@@ -1285,6 +1300,7 @@ $$$
 
 ---
 ### 2. dir: $.$ workspace: $production$
+` + "Change Summary: `Plan: 1 to add, 2 to change, 3 to destroy.`" + `
 <details><summary>Show Output</summary>
 
 $$$diff
@@ -1377,6 +1393,7 @@ func TestRenderProjectResults_PlansDeleted(t *testing.T) {
 						Workspace:  "production",
 						PlanSuccess: &models.PlanSuccess{
 							TerraformOutput: "tf out",
+							ChangeSummary:   "Plan: 1 to add, 2 to change, 3 to destroy.",
 							LockURL:         "lock-url",
 							RePlanCmd:       "re-plan cmd",
 							ApplyCmd:        "apply cmd",
